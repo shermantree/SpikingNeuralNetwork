21cd580162bdd910d2de29c5495247fbae941db2
shermantree <shermantree@snu.ac.kr>
2019-10-17 15:44:28 +0900

    basic snn
    basic snn operation added

--- engine/snn.py
+++ engine/snn.py
@@ -3,14 +3,24 @@ import torch
 
 class IdealSNN:
     def __init__(self, structure = [728, 10], pulsewidth = 100e-9, inputlength = 10e-6, cmem = 10e-12, vt = 0.5 ):
+        '''
 
+        :param structure:
+        :param pulsewidth:
+        :param inputlength:
+        :param cmem:
+        :param vt:
+        '''
         self.pulsewidth = pulsewidth
+        self.timesteplength = pulsewidth
         self.structure = structure
         self.vt = vt
         self.inputlength = inputlength
 
         self.weight = []
-
+        self.vmem = []
+        self.vout = []
+        self.input  = None
         self.reset()
 
 
@@ -22,8 +32,55 @@ class IdealSNN:
             self.vmem.append(torch.zeros((1,layer)))
             self.vout.append(torch.zeros((1, layer)))
 
+    def load_input(self, input):
+        self.input = input
 
-    def set_weight_ideal(self, weight):
-        pass
+    def set_weight_ideal(self, weightmatrix):
+        '''
+        :param weightmatrix: list of weight matrix
+        :return:
+        '''
+        assert (len(weightmatrix) == len(self.structure) - 1)
+        for w in weightmatrix:
+            self.weight.append(torch.tensor(w))
 
 
+    def run(self):
+        '''
+        @brief run snn for single image data
+        :return: classification result
+        '''
+        time = 0
+        # repeat this for the time an input signal is given
+        # do x*W (not W*x)
+        while time < self.inputlength:
+            for layernum in range(len(self.weight)):
+                if layernum == 0: # if first layer, use input signal
+                    temp = torch.mul(self.input[layernum][(int)(time/self.timesteplength)], self.weight[layernum])
+                    self.vmem[layernum] = torch.add(self.vmem[layernum], temp)
+                    for i in range(len(self.vmem[layernum])):
+
+                        if self.vmem[layernum][i] > self.vt:
+                            self.vout[layernum][i] = 1
+                            self.vmem[layernum][i] = 0
+                        elif self.vmem[layernum][i] < 0:
+                            self.vmem[layernum][i] = 0
+                else:
+                    temp = torch.mul(self.vout[layernum - 1], self.weight[layernum])
+                    self.vmem[layernum] = torch.add(self.vmem[layernum], temp)
+                    for i in range(len(self.vmem[layernum])):
+                        self.vout[layernum][i] = 0
+                        if self.vmem[layernum][i] > self.vt:
+                            self.vout[layernum][i] = 1
+                            self.vmem[layernum][i] = 0
+                        elif self.vmem[layernum][i] < 0:
+                            self.vmem[layernum][i] = 0
+
+            time += self.timesteplength
+
+        print (self.o)
+        return
+
+if __name__ == '__main__()':
+    snn = IdealSNN()
+    snn.run()
---
+++ util/dataloader.py
@@ -0,0 +1,10 @@
+import os
+class dataloader:
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        '''
+        @brief make input into left-justified PWM signal
+        '''
+        pass
